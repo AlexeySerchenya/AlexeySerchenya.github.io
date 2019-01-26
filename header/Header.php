e21b17ce5c1a2ebcadd83dc4facc62fdc2795b9c
Alexey Serchenya <AlexeySerchenya@users.noreply.github.com>
2019-01-27 01:51:48 +0300

    IMG

--- header/Header.php
+++ header/Header.php
@@ -1,5 +1,14 @@
 <body>
 <link rel="stylesheet" href="header/header.css">
-<img class="line" src="header/img/line.png">
-<p class="logo"><a href="index.php"></a></p>
+<p class="menu">
+    <a href="">1</a>
+    <a href="">2</a>
+    <a href="">3</a>
+    <a href="">4</a>
+    <a href="">5</a>
+    <a href="">6</a>
+    <a href="">7</a>
+</p>
+<a class="logo" href="index.php"><img src="header/img/logo.png" alt="главная"></a>
+<img class="line_botom" src="header/img/line.png">
 </body>
