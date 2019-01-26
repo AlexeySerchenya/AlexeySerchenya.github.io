afa1a1e5a7360c6303631ae9059f8217d9e1c012
Alexey Serchenya <AlexeySerchenya@users.noreply.github.com>
2019-01-27 01:22:12 +0300

    IMG

--- header/Header.php
+++ header/Header.php
@@ -1,4 +1,5 @@
 <body>
 <link rel="stylesheet" href="header/header.css">
-<img src="header/img/line.png">
+<img class="line" src="header/img/line.png">
+<p class="logo"><a href="index.php"></a></p>
 </body>
